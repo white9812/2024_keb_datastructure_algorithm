81855c505b8a9bdd38f333d144e7ceae14e0a5b4
white9812 <white9812@gmail.com>
2024-02-08 10:42:22 +0900

    v0.3

--- day01.py
+++ day01.py
@@ -1,13 +1,42 @@
-def factorial(number)->int:
-    result=1
-    for i in range(1,number+1):
-        result=result*i
+# def factorial(number) -> int:
+#     '''
+#     factorial by repetition
+#     :param number: number (int)
+#     :return: factorial result (int)
+#     '''
+#     result = 1
+#     for i in range(1, number+1):
+#         result = result * i
+#     return result
 
-def nCr(n,r)->int:
-    numerator=factorial(n)
-    denominator=factorial(n-r)*factorial(r)
-    return int(numerator/denominator)
-if __name__=="__main__":
-    n=int(input("Input n : "))
-    r=int(input("Input r : "))
-    print(f"{n}C{r}={nCr(n,r)}")
+
+def factorial(number) -> int:
+    '''
+    factorial by recursion
+    :param number: number (int)
+    :return: factorial result (int)
+    '''
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
+
+def nCr(n, r) -> int:
+    '''
+    조합 함수
+    :param n:
+    :param r:
+    :return:
+    '''
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return int(numerator / denominator)
+
+
+if __name__ == "__main__":
+    # n = int(input("Input n : "))
+    # r = int(input("Input r : "))
+    # print(f"{n}C{r} = {nCr(n, r)}")
+    f = int(input())
+    print(factorial(f))
