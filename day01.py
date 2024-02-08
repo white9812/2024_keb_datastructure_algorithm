b0823584fb660e40b4dc738dd4c843f5d208abed
white9812 <white9812@gmail.com>
2024-02-08 17:09:11 +0900

    v1.3 ãnodes

--- day01.py
+++ day01.py
@@ -1,34 +1,66 @@
-def print_poly(t_x,f_x) -> str:
 
-    poly_expression = "f(x) = "
+class Node() :
+    def __init__ (self) :
+        self.data = None
+        self.link = None
 
-    for i in range(len(fx)):
-        term=t_x[i]
-        coefficient = f_x[i]
-        #if coefficient !=0: <- 내 생각
+def print_nodes(start) :
+    current = start
+    if current is None :
+        return
+    print(current.data, end = ' ')
+    while current.link is not None:
+        current = current.link #다음으로 이동하게 해주는 코드
+        print(current.data, end = ' ')
+    print()
+def insert_node(find_data, insert_data) :
+    global head, current, pre
 
-        if coefficient >=0 and i !=0 :
-            poly_expression = poly_expression + "+"
-        poly_expression = poly_expression + f'{coefficient}x^{term} '
+    if head.data is find_data :      # 첫 번째 노드 삽입
+        node = Node()
+        node.data = insert_data
+        node.link = head #화사노드의 링크에 다현이 연결됨
+        head = node
+        return
+
+    current = head #다현부터 시작함
+    while current.link is not None :    # 중간 노드 삽입 #지효만 해당이 됨
+        pre = current #current의 주소를 pre가 받음
+        current = current.link
+        if current.data is find_data : #솔라랑 같은지 보는 것
+            node = Node()
+            node.data = insert_data
+            node.link = current
+            pre.link = node
+            return
+
+    node = Node()                   # 마지막 노드 삽입
+    node.data = insert_data #문별 넣고
+    current.link = node #끝까지 갔음, 문별의 링크는 none이 되어서 마지막까지 유지됨
+
+head, current, pre = None, None, None
+data_array = ["다현", "정연", "쯔위", "사나", "지효"]
+
+## 메인 코드 부분 ##
+if __name__ == "__main__" :
+
+    node = Node()		# 첫 번째 노드
+    node.data = data_array[0]
+    head = node
 
 
-    return poly_expression
+    for data in data_array[1:] :	# 두 번째 이후 노드
+        pre = node
+        node = Node()
+        node.data = data
+        pre.link = node
 
 
-def calculation_poly(x_value, t_x,f_x) -> int:
-    return_value = 0
-
-    for i in range(len(fx)):
-        term=t_x[i]
-        coefficient = f_x[i]
-        return_value += coefficient * pow(x_value, term)
-
-
-    return return_value
-
-tx=[300,20,0]
-fx = [7,-4,5]
-
-if __name__ == "__main__":
-    print(print_poly(tx,fx))
-    print(calculation_poly(int(input("x 값 : ")),tx, fx))
+    print_nodes(head) #이름들만 꺼내서 출력하도록 함
+    insert_node("다현", "화사")
+    print_nodes(head)
+    insert_node("사나", "솔라")
+    print_nodes(head)
+    insert_node("동균", "문별") #동균은 찾을 수 없음 지효까지 갔음 그래서 지효의 링크에
+                                                 #문별을 연결해줌
+    print_nodes(head)
