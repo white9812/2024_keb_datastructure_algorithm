481255ca6929627064a4058c368c75c7e7dcc29d
white9812 <white9812@gmail.com>
2024-02-08 15:05:00 +0900

    v0.8

--- day01.py
+++ day01.py
@@ -1,21 +1,32 @@
-import random
-answer=random.randint(1,100)
-chance=7
-print(answer)
-while chance !=0 :
-    guess=int(input("Input guess number: "))
-    if guess == answer:
-        print(f"You win. Answer is {answer}",chance)
-        break
-    elif guess > answer:
-        chance=chance-1
-        print(f"{guess} number is bigger than answer",chance)
-    else:
-        chance=chance-1
-        print(f"{guess} is lower than answer",chance)
-        #크다 작다를 알려주는 것은 중앙값을 이용하게 하여 수학적으로 이길 수 밖에 없게 됨
-        #힌트가 사라진다면 입력한 수가 맞는지 아닌지만 알게됨
-        #최악의 경우 100번째 만에 알게 될 수도 있음
-        #힌트가 사라지면 선형시간을 갖게 됨
-else:
-    print(f"you lost.Answer is {answer}")
+def print_poly(f_x) -> str:
+    term = len(f_x) - 1
+    poly_expression = "f(x) = "
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+        term = term - 1
+
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x) -> int:
+    return_value = 0
+    term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        return_value += coefficient * pow(x_value, term)
+        term = term - 1
+
+    return return_value
+
+
+fx = [2, 3, 4, 0, -9]
+
+if __name__ == "__main__":
+    print(print_poly(fx))
+    print(calculation_poly(int(input("x 값 : ")), fx))
