a97d4a840b68476574b0dfbf5a6963e9ace95401
white9812 <white9812@gmail.com>
2024-02-08 15:43:05 +0900

    v1.2 self

--- day01.py
+++ day01.py
@@ -1,36 +1,34 @@
-def print_poly(f_x) -> str:
-    term = len(f_x) - 1
+def print_poly(t_x,f_x) -> str:
+
     poly_expression = "f(x) = "
 
     for i in range(len(fx)):
+        term=t_x[i]
         coefficient = f_x[i]
         #if coefficient !=0: <- 내 생각
-        if coefficient == 0:
-            term = term - 1
-            continue # 계수가 0일때는 그냥 지나간다.
+
         if coefficient >=0 and i !=0 :
             poly_expression = poly_expression + "+"
         poly_expression = poly_expression + f'{coefficient}x^{term} '
-        term = term - 1
 
 
     return poly_expression
 
 
-def calculation_poly(x_value, f_x) -> int:
+def calculation_poly(x_value, t_x,f_x) -> int:
     return_value = 0
-    term = len(f_x) - 1
 
     for i in range(len(fx)):
+        term=t_x[i]
         coefficient = f_x[i]
         return_value += coefficient * pow(x_value, term)
-        term = term - 1
+
 
     return return_value
 
-
-fx = [2, 3, 4, 0, -9]
+tx=[300,20,0]
+fx = [7,-4,5]
 
 if __name__ == "__main__":
-    print(print_poly(fx))
-    print(calculation_poly(int(input("x 값 : ")), fx))
+    print(print_poly(tx,fx))
+    print(calculation_poly(int(input("x 값 : ")),tx, fx))
