98852e875a53ff8ebead820912c392d38f8c0813
white9812 <white9812@gmail.com>
2024-02-08 13:34:35 +0900

    v0.6 my

--- day01.py
+++ day01.py
@@ -1,12 +1,14 @@
-import mymath
-import time
+import random
+answer=random.randrange(100)
+number=int(input())
+if number==answer :
+    print ("정답입니다. 시도횟수 ",1)
+elif number!=answer:
+        print("틀렸습니다. 숫자를 다시 입력하세요.")
+        count=0
+        while(number1!=answer):
+            number1=int(input())
+            count++
+            print("틀렸습니다. 시도횟수 ", count)
 
-
-if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-
-    print(f"{n}C{r} = {mymath.nCr(n, r)}")
-
-    #f = int(input())
-    #print(factorial(f))
+            break number
