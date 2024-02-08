98a4bf0ae5f7559fe95dbf0c9b541dcbf7e43f23
white9812 <white9812@gmail.com>
2024-02-08 10:27:03 +0900

    v0.2

--- day01.py
+++ day01.py
@@ -1,3 +1,12 @@
+def factorial(number)->int:
+    result=1
+    for i in range(1,number+1):
+        result=result*i
+
+def nCr(n,r)->int:
+    numerator=factorial(n)
+    denominator=factorial(n-r)*factorial(r)
+    return int(numerator/denominator)
 if __name__=="__main__":
     n=int(input("Input n : "))
     r=int(input("Input r : "))
