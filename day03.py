fcaba2aaf82bfcae6b29c4a93e9c51337a73e2ac
white9812 <white9812@gmail.com>
2024-02-14 16:02:44 +0900

    v0.4

--- day03.py
+++ day03.py
@@ -1,36 +1,26 @@
-class Graph() :
-	def __init__ (self, size) :
-		self.SIZE = size
-		self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
-def printGraph(g) :
-    print(' ', end = ' ')
-    for v in range(g.SIZE) :
-        print(nameAry[v], end = ' ')
-    print()
-    for row in range(g.SIZE) :
-        print(nameAry[row], end = ' ')
-        for col in range(g.SIZE) :
-            print(g.graph[row][col], end= '  ')
-        print()
-    print()
+'''
+G1=None
+nameAry=["문별","솔라","휘인","쯔위","선미","화사"]
+mb,sl,hi,zz,sm,hs=0,1,2,3,4,5
+'''
+#깊이 우선 탐색
+def dfs(g,v,visited):
+    visited[v]=1
+    print(chr(ord("A")+v),end=' ')
+    for i in range(len(g)):
+        if graph[v][i] ==1 and not visited[i]: #왜 1이 아니라 True? 1일때랑 true 일때랑 다름
+            dfs(g,i,visited)
+graph=[
+    [0,1,1,0,0,0], #문별
+    [1,0,0,1,0,0], #솔라
+    [1,0,0,1,0,0], #휘인
+    [0,1,1,0,1,1], #쯔위
+    [0,0,0,1,0,1], #선미
+    [0,0,0,1,1,0]  #화사
+]
 
+visited=[0 for _ in range(len(graph))]
+visited=[0]*len(graph)
+dfs(graph,0,visited)
 
-## 전역 변수 선언 부분 ##
-G1 = None
-nameAry = ['문별', '솔라', '휘인', '쯔위', '선미', '화사']
-mb, sl, hi, zi, sm, hs = 0, 1, 2, 3, 4, 5
-
-
-## 메인 코드 부분 ##
-gSize = 6
-G1 = Graph(gSize)
-G1.graph[mb][sl] = 1; G1.graph[mb][hi] = 1
-G1.graph[sl][mb] = 1; G1.graph[sl][zi] = 1
-G1.graph[hi][mb] = 1; G1.graph[hi][zi] = 1
-G1.graph[zi][sl] = 1; G1.graph[zi][hi] = 1; G1.graph[zi][sm] = 1; G1.graph[zi][hs] = 1
-G1.graph[sm][zi] = 1; G1.graph[sm][hs] = 1
-G1.graph[hs][zi] = 1; G1.graph[hs][sm] = 1
-
-print('## G1 무방향 그래프 ##')
-printGraph(G1)
