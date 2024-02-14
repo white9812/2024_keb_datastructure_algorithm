9637b671ab79dbe51db5ab3b3d7e7dcd1a25bd7e
white9812 <white9812@gmail.com>
2024-02-14 16:32:23 +0900

    v0.7 recursion

--- day03.py
+++ day03.py
@@ -7,7 +7,7 @@ def decimal_to_octal(number:int)-> str:
     if number < 8:
         return str(number)
     else:
-        return decimal_to_octal(n//8) +str(n%8)
+        return decimal_to_octal(number//8) +str(number%8)
     # octal=" "
     # while number > 0:
     #     octal = str(number % 8) + octal
