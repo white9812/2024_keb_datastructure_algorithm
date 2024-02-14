a410715c56ec6570edc70df7e4181f8878355d84
white9812 <white9812@gmail.com>
2024-02-14 11:39:21 +0900

    v0.2

--- day03.py
+++ day03.py
@@ -1,83 +1,25 @@
-## 함수 선언 부분 ##
-class TreeNode():
-    def __init__(self):
-        self.left = None
-        self.data = None
-        self.right = None
+## 함수 선언부 예제문제
+class Graph:
+    def __init__ (self, size) :
+        self.SIZE = size
+        self.graph = [ [0 for _ in range(size)] for _ in range(size) ]
+g=Graph(3)
+print(g.graph) # 3x3 matrix
 
+# # 전역 변수부
+# G1, G3 = None, None
+#
+# 메인 코드부
+G1 = Graph(4) #4x4
+G1.graph[0][3] = 1
+G1.graph[1][2] = 1
+G1.graph[1][3] = 1
+G1.graph[2][1] = 1
+G1.graph[3][0] = 1
+G1.graph[3][1] = 1
 
-## 전역 변수 선언 부분 ##
-memory = []
-root = None
-nameAry = ['블랙핑크', '레드벨벳', '마마무', '에이핑크', '걸스데이', '트와이스']
-#블랙핑크-(레드벨벳-걸스데이,마마무),에이핑크-트와이스-에이핑크 지우려면 지우려고 하는 current에 이젠 블랙핑크랑 트와이스를 연결
-## 메인 코드 부분 ##
-node = TreeNode()
-node.data = nameAry[0]
-root = node
-memory.append(node)
-
-for name in nameAry[1:]:
-
-    node = TreeNode()
-    node.data = name
-
-    current = root
-    while True:
-        if name < current.data:
-            if current.left == None:
-                current.left = node
-                break
-            current = current.left
-        else:
-            if current.right == None:
-                current.right = node
-                break
-            current = current.right
-
-    memory.append(node)
-
-deleteName = '마마무'
-
-current = root
-parent = None
-while True:
-    if deleteName is current.data:
-
-        if current.left is None and current.right is None:
-            if parent.left == current:
-                parent.left = None
-            else:
-                parent.right = None
-            del current
-        #자식이 없을때
-        elif current.left is not None and current.right is  None:
-            if parent.left == current:
-                parent.left = current.left
-            else:
-                parent.right = current.left
-            del current
-
-        elif current.left is None and current.right is not None:
-            if parent.left == current:
-                parent.left = current.right
-            else:
-                # print(current.data)
-                # print(current.right.data)
-                parent.right = current.right #삭제 할 노드(에이핑크 노드)의 오른쪽 링크 노드(트와이스 노드)를 블랙핑크 오른쪽 링크에 연결
-            del current
-
-        print(deleteName, '이(가) 삭제됨.')
-        break
-    elif deleteName < current.data:
-        if current.left == None:
-            print(deleteName, '이(가) 트리에 없음')
-            break
-        parent = current
-        current = current.left
-    else: #양쪽에 자식 노드가 있을때 처리하는 코드가 필요함
-        if current.right == None:
-            print(deleteName, '이(가) 트리에 없음')
-            break
-        parent = current
-        current = current.right
+print('##  G1 무방향 그래프 ##')
+for row in range(G1.SIZE):
+    for col in range(G1.SIZE): #4라고 바로 쓰는거 보다는 정수 리터럴을 쓴ㄴ게 좋음
+        print(G1.graph[row][col], end=' ')
+    print() # 행이 바뀌는 시점에는 줄바꿈하기
