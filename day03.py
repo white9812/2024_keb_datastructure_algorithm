ddeb9fcda7eeb96eb4774ddf225adae8d5471100
white9812 <white9812@gmail.com>
2024-02-14 14:05:16 +0900

    v0.3

--- day03.py
+++ day03.py
@@ -1,25 +1,36 @@
-## 함수 선언부 예제문제
-class Graph:
-    def __init__ (self, size) :
-        self.SIZE = size
-        self.graph = [ [0 for _ in range(size)] for _ in range(size) ]
-g=Graph(3)
-print(g.graph) # 3x3 matrix
+class Graph() :
+	def __init__ (self, size) :
+		self.SIZE = size
+		self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
-# # 전역 변수부
-# G1, G3 = None, None
-#
-# 메인 코드부
-G1 = Graph(4) #4x4
-G1.graph[0][3] = 1
-G1.graph[1][2] = 1
-G1.graph[1][3] = 1
-G1.graph[2][1] = 1
-G1.graph[3][0] = 1
-G1.graph[3][1] = 1
+def printGraph(g) :
+    print(' ', end = ' ')
+    for v in range(g.SIZE) :
+        print(nameAry[v], end = ' ')
+    print()
+    for row in range(g.SIZE) :
+        print(nameAry[row], end = ' ')
+        for col in range(g.SIZE) :
+            print(g.graph[row][col], end= '  ')
+        print()
+    print()
 
-print('##  G1 무방향 그래프 ##')
-for row in range(G1.SIZE):
-    for col in range(G1.SIZE): #4라고 바로 쓰는거 보다는 정수 리터럴을 쓴ㄴ게 좋음
-        print(G1.graph[row][col], end=' ')
-    print() # 행이 바뀌는 시점에는 줄바꿈하기
+
+## 전역 변수 선언 부분 ##
+G1 = None
+nameAry = ['문별', '솔라', '휘인', '쯔위', '선미', '화사']
+mb, sl, hi, zi, sm, hs = 0, 1, 2, 3, 4, 5
+
+
+## 메인 코드 부분 ##
+gSize = 6
+G1 = Graph(gSize)
+G1.graph[mb][sl] = 1; G1.graph[mb][hi] = 1
+G1.graph[sl][mb] = 1; G1.graph[sl][zi] = 1
+G1.graph[hi][mb] = 1; G1.graph[hi][zi] = 1
+G1.graph[zi][sl] = 1; G1.graph[zi][hi] = 1; G1.graph[zi][sm] = 1; G1.graph[zi][hs] = 1
+G1.graph[sm][zi] = 1; G1.graph[sm][hs] = 1
+G1.graph[hs][zi] = 1; G1.graph[hs][sm] = 1
+
+print('## G1 무방향 그래프 ##')
+printGraph(G1)
