3e5aabe1ad7a1d0b5bb6a5f77a39494ca0e45740
white9812 <white9812@gmail.com>
2024-02-15 11:20:46 +0900

    v0.2 fibonacci after gui

--- day04.py
+++ day04.py
@@ -14,20 +14,30 @@ def fibo_memoization(n:int)-> int:
     result = fibo_memoization(n-1)+fibo_memoization(n-2)
     memo[n]=result
     return result
-w=tk.Tk() #create window object
-w.title("Fibonacci")
-w.geometry("250x100")
-#create widget
-lbl_display_fibonacci_result=tk.Label(w,text="Fibonacci by memoization")
-en_input_number=tk.Entry(w)#입력상자 만들기
-btn_click=tk.Button(w,text="Click") #여기까진 안올라갔음
+def process_fibonacci():
+    number = int(en_input_number.get())#input
+    # lbl_display_fibonacci_result.config(text=f"{number+1}") #display
+    lbl_display_fibonacci_result.config(text=f"fibonacci({number})={fibo_memoization(number)}")  # display
+#   print(number+1) 할때는 console창에 올라옴
 
-#layout
-lbl_display_fibonacci_result.pack()
-en_input_number.pack(fill="x")
-btn_click.pack(fill="x")
 
-w.mainloop()
+if __name__=="__main__":
+    w=tk.Tk() #create window object
+    w.title("Fibonacci")
+    w.geometry("250x100")
+    #create widget
+    lbl_display_fibonacci_result=tk.Label(w,text="Fibonacci by memoization")
+    en_input_number=tk.Entry(w)#입력상자 만들기
+    btn_click=tk.Button(w,text="Click",command=process_fibonacci) #여기까진 안올라갔음
+    #bind function
+
+    #layout
+    lbl_display_fibonacci_result.pack()
+    en_input_number.pack(fill="x")
+    btn_click.pack(fill="x")
+    en_input_number.focus()
+
+    w.mainloop()
 
 
 # n=int(input("Int number: ")) # Input box
