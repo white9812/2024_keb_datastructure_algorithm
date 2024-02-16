cf8282249d4be4dc2c599fca97553df837ad38c5
white9812 <white9812@gmail.com>
2024-02-16 14:56:31 +0900

    fiself 4-1

--- assignment.py
+++ assignment.py
@@ -1,18 +1,75 @@
-memo=[0,1]
-count=0
-def fibo_memoization(n:int)-> int:
+## 클래스와 함수 선언 부분 ##
+class Node() :
+	def __init__ (self) :
+		self.data = None
+		self.link = None
 
-    '''
-    fibonacci function by recursion with memoization
-    :param n: integer number
-    :return: integer number
-    '''
-    global memo ,count
+def printNodes(start) :
+	current = start
+	if current == None :
+		return
+	print(current.data, end = ' ')
+	while current.link != None:
+		current = current.link
+		print(current.data, end = ' ')
+	print()
 
-    for i in range(0,n+1):
-        memo.append(memo[i]+memo[i+1])
-        count+=1
-    return memo[n-1],count
+def deleteNode(deleteData) :
+	global memory, head, current, pre
+
+	if head.data == deleteData :         # 첫 번째 노드 삭제
+		current = head
+		head = head.link
+		del(current)
+		print("첫번째 노드 삭제")
+		return
+
+	current = head                          # 첫 번째  외 노드 삭제
+	while current.link != None :
+		pre = current
+		current = current.link
+		if current.data == deleteData :
+			pre.link = current.link
+			del(current)
+			print("중간 노드 삭제")
+			return
+
+		print("삭제된 노드 없음")
+
+## 전역 변수 선언 부분 ##
+memory = []
+head, current, pre = None, None, None
+dataArray = ["다현", "정연", "쯔위", "사나", "지효"]
+
+## 메인 코드 부분 ##
+if __name__ == "__main__" :
+
+	node = Node()			# 첫 번째 노드
+	node.data = dataArray[0]
+	head = node
+	memory.append(node)
+
+	for data in dataArray[1:] :		# 두 번째 노드부터
+		pre = node
+		node = Node()
+		node.data = data
+		pre.link = node
+		memory.append(node)
+
+	printNodes(head)
+
+	deleteNode("다현")
+	printNodes(head)
+
+	deleteNode("쯔위")
+	printNodes(head)
+
+	deleteNode("지효")
+	printNodes(head)
+
+	deleteNode("재남")
+	printNodes(head)
+
 
 n=int(input("Int number: "))
 
