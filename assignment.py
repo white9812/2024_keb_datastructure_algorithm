cbb673860610fee144e31021ec43459361f73852
white9812 <white9812@gmail.com>
2024-02-16 14:59:59 +0900

    self 4-3

--- assignment.py
+++ assignment.py
@@ -14,63 +14,44 @@ def printNodes(start) :
 		print(current.data, end = ' ')
 	print()
 
-def deleteNode(deleteData) :
+def makeSimpleLinkedList(namePhone) :
 	global memory, head, current, pre
+	printNodes(head)
 
-	if head.data == deleteData :         # 첫 번째 노드 삭제
-		current = head
-		head = head.link
-		del(current)
-		print("첫번째 노드 삭제")
+	node = Node()
+	node.data = namePhone
+	memory.append(node)
+	if head == None :			# 첫 번째 노드일 때
+		head = node
 		return
 
-	current = head                          # 첫 번째  외 노드 삭제
+	if head.data[0] > namePhone[0] :	# 첫 번째 노드보다 작을 때
+		node.link = head
+		head = node
+		return
+
+	# 중간 노드로 삽입하는 경우
+	current = head
 	while current.link != None :
 		pre = current
 		current = current.link
-		if current.data == deleteData :
-			pre.link = current.link
-			del(current)
-			print("중간 노드 삭제")
+		if current.data[0] > namePhone[0]:
+			pre.link = node
+			node.link = current
 			return
 
-		print("삭제된 노드 없음")
+	# 삽입하는 노드가 가장 큰 경우
+	current.link = node
 
 ## 전역 변수 선언 부분 ##
 memory = []
 head, current, pre = None, None, None
-dataArray = ["다현", "정연", "쯔위", "사나", "지효"]
+dataArray = [["지민", "180"], ["정국", "177"], ["뷔", "183"], ["슈가", "175"], ["진", "179"]]
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__" :
 
-	node = Node()			# 첫 번째 노드
-	node.data = dataArray[0]
-	head = node
-	memory.append(node)
-
-	for data in dataArray[1:] :		# 두 번째 노드부터
-		pre = node
-		node = Node()
-		node.data = data
-		pre.link = node
-		memory.append(node)
+	for data in dataArray :
+		makeSimpleLinkedList(data)
 
 	printNodes(head)
-
-	deleteNode("다현")
-	printNodes(head)
-
-	deleteNode("쯔위")
-	printNodes(head)
-
-	deleteNode("지효")
-	printNodes(head)
-
-	deleteNode("재남")
-	printNodes(head)
-
-
-n=int(input("Int number: "))
-
-print(fibo_memoization(n))
